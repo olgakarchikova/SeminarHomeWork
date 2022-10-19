cca9dcb8c2edcd81bf9ba1295be2af877e20c257
Karchikova_Olga <olgasolo1@yandex.ru>
2022-10-19 16:42:32 +0300

    Task_4 added

--- HomeWork001/Program.cs
+++ HomeWork001/Program.cs
@@ -63,3 +63,19 @@ else
     Console.WriteLine("No");
 }
 */
+
+/* Задача 4. Напишите программу, которая на входе принимает число (N), а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int carrent = 1;
+
+while(carrent <= N)
+{
+    if(carrent % 2 == 0)
+    {
+        Console.Write(carrent + " ");
+    }    
+    carrent ++;
+}
+*/
