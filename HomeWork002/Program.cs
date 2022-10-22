05a2125f63a7f1b1b6684b9b9c15f97e7fe66e3c
Karchikova_Olga <olgasolo1@yandex.ru>
2022-10-22 21:27:25 +0300

    HomeWork002 added

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,10 @@
+﻿int CutNumber(int num)
+{
+    int sot = (num / 10) % 10;
+    num = sot;
+    return num;
+}
+
+int number = new Random().Next(100, 1000);
+int result = CutNumber (number);
+Console.WriteLine($"second digit of number {number} is {result}");
