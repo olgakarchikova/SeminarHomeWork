bd576eef17518d24daaf6c4ee72155197cabcf90
Karchikova_Olga <olgasolo1@yandex.ru>
2022-10-23 13:23:59 +0300

    HomeWork002 Task1 added

--- HomeWork002/Program.cs
+++ HomeWork002/Program.cs
@@ -1,10 +1,17 @@
-﻿int CutNumber(int num)
+﻿/*
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int CutNumber(int num)
 {
-    int sot = (num / 10) % 10;
-    num = sot;
+    int secdigit = (num / 10) % 10;
+    num = secdigit;
     return num;
 }
 
 int number = new Random().Next(100, 1000);
 int result = CutNumber (number);
 Console.WriteLine($"second digit of number {number} is {result}");
+*/
+
+// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
