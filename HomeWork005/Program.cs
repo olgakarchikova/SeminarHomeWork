7e40bec042f74e0561be29b1e84114b432e9708d
Karchikova_Olga <olgasolo1@yandex.ru>
2022-11-16 00:30:22 +0300

    ДЗ к 5 семинару

---
+++ HomeWork005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] CreateRandomeArray(int size, int minValue, int maxValue) 
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i ++)
+        array[i] = new Random().Next(minValue, maxValue +1);
+    return array;
+}
+void ShowArray(int [] array) 
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
