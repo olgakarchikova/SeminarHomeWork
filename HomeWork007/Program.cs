cb3c2434d876c042a8d0b9260626ea3e826777dc
Karchikova_Olga <olgasolo1@yandex.ru>
2022-11-25 02:48:23 +0300

    Task 47 added

---
+++ HomeWork007/Program.cs
@@ -0,0 +1,79 @@
+﻿//Задача47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
+double[,] CreateRandom2Array(int rows, int colums)
+{
+    double [,] array = new double[rows, colums];
+
+    for (int i = 0; i < rows; i ++)
+        for (int j = 0; j < colums; j++)
+            array[i,j] = new Random().NextDouble() + new Random().Next(-10,10);
+    return array;
+}
+void Show2Array(double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+         for (int j = 0; j < array.GetLength(1); j++)
+         {
+            double align = Math.Round(array[i,j],1);
+            Console.Write(align + " ");
+         }   
+        Console.WriteLine(); 
+    }
+}
+
+Console.WriteLine("Input a number of rows");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a number of colums");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] myArray = CreateRandom2Array(m, n);
+Show2Array(myArray);
+*/
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+//в двумерном массиве, и возвращает значение этого элемента или же указание, 
+//что такого элемента нет.
+
+int [,] CreateRandom2Array(int rows, int colums)
+{
+    int [,] array = new int[rows, colums];
+
+    for (int i = 0; i < rows; i ++)
+        for (int j = 0; j < colums; j++)
+            array[i,j] = new Random().Next(1,10);
+    return array;
+}
+void Show2Array(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+         for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        Console.WriteLine(); 
+    }
+}
+
+bool FindElements(int [,] array, int a, int b)
+{
+     int [,] numArray = new int [a,b]; 
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array [i,j] == numArray[a,b])
+            return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Input a number of rows");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a number of colums");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the row index");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the column index");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int [,] myArray = CreateRandom2Array(m, n);
+Show2Array(myArray);
+FindElements(myArray, row, column);
